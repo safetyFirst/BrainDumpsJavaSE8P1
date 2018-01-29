fdc3f87d33f5820faa913a3e29eb2492a40aa4f3
Marco Wagner <marco1989wagner@gmail.com>
2018-01-29 14:33:14 +0100

    add more exam code

--- src/bd86/Test.java
+++ src/bd86/Test.java
@@ -1,10 +1,12 @@
 package bd86;
 
-public class Test {
+final class Test {
 
     public static void main(String[] args) {
         boolean a = new Boolean(Boolean.valueOf(args[0]));
         boolean b = new Boolean(args[1]);
         System.out.println(a + " " + b);
     }
+
+
 }
